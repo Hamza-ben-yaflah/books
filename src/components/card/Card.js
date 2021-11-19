2aaa933892e54f7fb48cd6fb798850454c9f26ba
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2021-11-19 16:31:01 +0100

    css style

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,7 @@ import "./Card.css";
 function Card({ inf }) {
   console.log();
   return (
-    <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
+    /*<article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw5 center">
       <img
         src={inf.formats["image/jpeg"]}
         className="db w-100 br2 br--top"
@@ -19,7 +19,25 @@ function Card({ inf }) {
           </div>
         </div>
       </div>
-    </article>
+    </article>*/
+    <div class="card">
+      <div class="imgBox">
+        <img
+          src={inf.formats["image/jpeg"]}
+          alt="mouse corsair"
+          class="mouse"
+        />
+      </div>
+      <div class="contentBox">
+        <h3>{inf.title}</h3>
+        <h2 class="price">
+          id.<small>{inf.id}</small>
+        </h2>
+        <a href="google.com" class="buy">
+          go to description
+        </a>
+      </div>
+    </div>
   );
 }
 
